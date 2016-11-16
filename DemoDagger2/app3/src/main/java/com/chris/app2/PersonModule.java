05413468bc3df8b93e9a55f4c48120d54350111f
mocanis <mocanis@163.com>
2016-11-16 22:59:31 +0800

    init

---
+++ DemoDagger2/app3/src/main/java/com/chris/app2/PersonModule.java
@@ -0,0 +1,19 @@
+package com.chris.app2;
+
+import dagger.Module;
+import dagger.Provides;
+
+/**
+ * Email: suoyiman@163.coom
+ */
+@Module
+public class PersonModule {
+    @Provides
+    public Person providePerson(String name){
+        return new Person(name);
+    }
+    @Provides
+    public String provideName(){
+        return "suoyiman";
+    }
+}
